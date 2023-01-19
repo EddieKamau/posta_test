bf6b3049a8733186fb6bc0c0bdb75359b02e6e5e
Eddie Genius <elyudde@gmail.com>
2023-01-19 11:00:13 +0300

    backend init

---
+++ backend/lib/backend.dart
@@ -0,0 +1,11 @@
+/// backend
+///
+/// A conduit web server.
+library backend;
+
+export 'dart:async';
+export 'dart:io';
+
+export 'package:conduit_core/conduit_core.dart';
+
+export 'channel.dart';
